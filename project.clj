c58419242162eb386efe9d9eb25d7fccf209271c
stubillwhite <stubillwhite@gmail.com>
2015-11-15 18:03:36 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -4,31 +4,22 @@
 
   :url "https://github.com/stubillwhite/project-euler"
 
-  :license { :name "Eclipse Public License"
-             :url "http://www.eclipse.org/legal/epl-v10.html" }
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html" }
 
-  :repl-options { :port 4555 }
+   :repl-options { :port 4555 }
 
-  :plugins [ [cider/cider-nrepl "0.8.2"]
-             [lein-expectations "0.0.7"]
-             [lein-autoexpect "1.4.0"] ]
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/core.async "0.2.374"]
+                 [org.clojure/math.numeric-tower "0.0.4"] 
 
-  :dependencies [
-                  ;; Core
-                  [org.clojure/clojure "1.6.0"]
-                  [org.clojure/core.async "0.1.319.0-6b1aca-alpha"]
-                  [org.clojure/math.numeric-tower "0.0.4"] 
-
-                  ;; Logging
-                  [com.taoensso/timbre "3.3.1"]
-                  
-                  ;; Debugging
-                  [org.clojure/tools.trace "0.7.8"]
-                  
-                  ;; Graphing
-                  [org.jfree/jfreechart "1.0.19"]
-                  [org.jfree/jcommon "1.0.23"] ]
+                 ;; Logging
+                 [com.taoensso/timbre "4.1.4"]
+                 [org.clojure/tools.trace "0.7.9"]
+                 
+                 [org.jfree/jfreechart "1.0.19"]
+                 [org.jfree/jcommon "1.0.23"]]
   
-  :profiles { :dev { :dependencies [ [org.clojure/tools.namespace "0.2.10"]
-                                     [expectations "2.1.0"] ]
-                     :source-paths ["dev"] } })
+  :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]
+                                  [expectations "2.1.3"]]
+                   :source-paths ["dev"]}})
