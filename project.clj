43091112b445f9eb8422466dcbdd6524ac7b8473
stubillwhite <stubillwhite@gmail.com>
2016-04-05 18:47:32 +0100

    Problem 0012 solution

--- project.clj
+++ project.clj
@@ -7,19 +7,19 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html" }
 
-   :repl-options { :port 4555 }
+  :repl-options {:port 4555}
 
-  :dependencies [[org.clojure/clojure "1.7.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
                  [org.clojure/math.numeric-tower "0.0.4"] 
 
                  ;; Logging
-                 [com.taoensso/timbre "4.1.4"]
+                 [com.taoensso/timbre "4.2.1"]
                  [org.clojure/tools.trace "0.7.9"]
                  
                  [org.jfree/jfreechart "1.0.19"]
                  [org.jfree/jcommon "1.0.23"]]
   
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]
-                                  [expectations "2.1.3"]]
+                                  [expectations "2.1.4"]]
                    :source-paths ["dev"]}})
