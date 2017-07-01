5dd472b41b2fd047b3110637178175fa7c69d9bd
Stuart White <s.white.1@elsevier.com>
2017-07-01 16:46:29 +0100

    Update dependencies

--- project.clj
+++ project.clj
@@ -10,18 +10,18 @@
   :repl-options {:port 4555}
 
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.385"]
+                 [org.clojure/core.async "0.2.391"]
                  [org.clojure/math.numeric-tower "0.0.4"] 
-                 [clojure-csv/clojure-csv "2.0.1"]
+                 [clojure-csv/clojure-csv "2.0.2"]
                  
                  ;; Logging
-                 [com.taoensso/timbre "4.6.0"]
+                 [com.taoensso/timbre "4.7.4"]
                  [org.clojure/tools.trace "0.7.9"]
 
                  ;; Analysing execution time
                  [org.jfree/jfreechart "1.0.19"]
                  [org.jfree/jcommon "1.0.23"]
-                 [com.taoensso/tufte "1.0.0-RC2"]]
+                 [com.taoensso/tufte "1.0.2"]]
   
   :profiles {:dev {:dependencies [[org.clojure/tools.namespace "0.2.10"]]
                    :source-paths ["dev"]}})
